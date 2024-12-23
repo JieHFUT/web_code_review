79d70f4d1ddc9a4d19c7eadcd3c3c4c1fd797775
JieCHERY <9594523wrj@gmail.com>
2024-12-23 19:15:42 +0800

    headline

--- r-web-headline/headline/src/com/jiehfut/headline/controller/PortalController.java
+++ r-web-headline/headline/src/com/jiehfut/headline/controller/PortalController.java
@@ -11,6 +11,7 @@ package com.jiehfut.headline.controller;
  */
 
 import com.jiehfut.headline.common.Result;
+import com.jiehfut.headline.pojo.HeadlineQueryVo;
 import com.jiehfut.headline.pojo.NewsType;
 import com.jiehfut.headline.service.NewsTypeService;
 import com.jiehfut.headline.service.NewsUserService;
@@ -38,6 +39,20 @@ public class PortalController extends BaseController {
     /**
      * 查询所有的新闻类型的业务接口实现
      * @param req
+     * @param resp
+     * @throws ServletException
+     * @throws IOException
+     */
+    protected void findAllTypes(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // 查询所有的新闻类型，装入 Result 对象响应给前端
+        List<NewsType> newsTypeList = newsTypeService.findAll();
+        WebUtil.writeJson(resp, Result.ok(newsTypeList));
+    }
+
+
+    /**
+     * 用户在成功登陆后，跳转到首页面，分页带条件查询所有头条
+     * @param req
      * {
      *     "keyWords":"马斯克", // 搜索标题关键字
      *     "type":0,           // 新闻类型
@@ -53,7 +68,7 @@ public class PortalController extends BaseController {
      *     		"pageData":[                           // 本页的数据
      *     			                {
      *     				"hid":"1",                     // 新闻id
-     *     				"title":"尚硅谷宣布 ... ...",   // 新闻标题
+     *     				"title":"宣布 ... ...",   // 新闻标题
      *     				"type":"1",                    // 新闻所属类别编号
      *     				"pageViews":"40",              // 新闻浏览量
      *     				"pastHours":"3" ,              // 发布时间已过小时数
@@ -61,7 +76,7 @@ public class PortalController extends BaseController {
      * 				},
      * 				{
      *     				"hid":"1",                     // 新闻id
-     *     				"title":"尚硅谷宣布 ... ...",   // 新闻标题
+     *     				"title":"宣布 ... ...",   // 新闻标题
      *     				"type":"1",                    // 新闻所属类别编号
      *     				"pageViews":"40",              // 新闻浏览量
      *     				"pastHours":"3",              // 发布时间已过小时数
@@ -69,7 +84,7 @@ public class PortalController extends BaseController {
      * 				},
      * 				{
      *     				"hid":"1",                     // 新闻id
-     *     				"title":"尚硅谷宣布 ... ...",   // 新闻标题
+     *     				"title":"宣布 ... ...",   // 新闻标题
      *     				"type":"1",                    // 新闻所属类别编号
      *     				"pageViews":"40",              // 新闻浏览量
      *     				"pastHours":"3",               // 发布时间已过小时数
@@ -86,21 +101,13 @@ public class PortalController extends BaseController {
      * @throws ServletException
      * @throws IOException
      */
-    protected void findAllTypes(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        // 查询所有的新闻类型，装入 Result 对象响应给前端
-        List<NewsType> newsTypeList = newsTypeService.findAll();
-        WebUtil.writeJson(resp, Result.ok(newsTypeList));
-    }
-
-
-    /**
-     * 用户在成功登陆后，跳转到首页面，分页带条件查询所有头条
-     * @param req
-     * @param resp
-     * @throws ServletException
-     * @throws IOException
-     */
     protected void findNewsPage(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // 接收请求中的参数
+        HeadlineQueryVo headlineQueryVo = WebUtil.readJson(req, HeadlineQueryVo.class);
+        
+        // 将参数传递给服务层进行分页查询
+
+        // 分页查询的结果转换为 JSON 相应给客户端
 
         WebUtil.writeJson(resp, Result.ok(null));
     }
